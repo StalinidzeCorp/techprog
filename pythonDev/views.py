954b3dbc1b270d25cc900e8d39d0a1b83b935492
StalinidzeCorp <guidemaster228@gmail.com>
2024-01-18 13:37:43 +0500

    some more styles and bugfixes

--- pythonDev/views.py
+++ pythonDev/views.py
@@ -34,12 +34,24 @@ def latest(request):
         else:
             skills = '-'
 
+        sal = '-'
+        cur = ''
+        if i['salary']:
+            cur = i['salary']['currency']
+            if i['salary']['from'] and not i['salary']['to']:
+                sal = f"от {'{0:,}'.format(i['salary']['from']).replace(',', ' ')}"
+            elif not i['salary']['from'] and i['salary']['to']:
+                sal = f"до {'{0:,}'.format(i['salary']['to']).replace(',', ' ')}"
+            else:
+                sal = f"{'{0:,}'.format(i['salary']['from']).replace(',', ' ')} - {'{0:,}'.format(i['salary']['to']).replace(',', ' ')}"
+
         item = {
             'name': i['name'] if i['name'] else '-',
             'description': r_v['description'] if r_v['description'] else '-',
             'skills': skills,
             'company': i['employer']['name'] if i['employer']['name'] else '-',
-            'salary': f"{'{0:,}'.format(i['salary']['from']).replace(',', ' ')} - {'{0:,}'.format(i['salary']['to']).replace(',', ' ')}" if i['salary'] else '-',
+            'salary': sal,
+            'currency': cur,
             'address': i['area']['name'] if i['area'] and i['area']['name'] else '-',
             'published_at': datetime.strptime(i['published_at'], "%Y-%m-%dT%H:%M:%S%z").strftime("%d.%m.%Y %H:%M") if i['published_at'] else '-',
             'url': i['alternate_url'] if i['alternate_url'] else '#'
